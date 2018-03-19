a6246a6cf4861b8a3c1348976d8d105989a3f6d3
Sadie <sadiefreeman@gmail.com>
2018-03-19 21:51:07 +0000

    downloaded future jpg, then added console logs for user experience

---
+++ httpreq.js
@@ -0,0 +1,16 @@
+var request = require('request');
+var fs = require('fs');
+
+console.log('Downloading image');
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+.on('error', function (err) {
+  throw err;
+})
+.on('response', function (response) {
+  console.log('Download complete');
+  console.log('Response Status Code: ', response.statusCode);
+  console.log('Response Message: ', response.statusMessage);
+  console.log('Response Content Type ', response.headers['content-type']);
+})
+
+.pipe(fs.createWriteStream('./future.jpg'))
